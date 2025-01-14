8ef9d24110d595e0464eaf8c778a3e1c6996ff37
Libero Lance <mfc2294@gmail.com>
2025-01-14 07:34:49 -0500

    add dynamic html code to page mod

---
+++ src/page.js
@@ -0,0 +1,9 @@
+const content = getElementById("content");
+
+const contentImg = document.createElement("img");
+const contentH = document.createElement("h1");
+const contentP = document.createElement("p");
+
+content.appendChild(contentImg);
+content.appendChild(contentH);
+content.appendChild(contentP);
