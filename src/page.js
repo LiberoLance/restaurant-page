1d65a30b1dcc28c6599c8916b680d55afb87c896
Libero Lance <mfc2294@gmail.com>
2025-01-16 09:46:51 -0500

    set headline text for page

--- src/page.js
+++ src/page.js
@@ -5,6 +5,8 @@ const contentH = document.createElement("h1");
 const contentP = document.createElement("p");
 
 contentImg.setAttribute("alt", "A plate of fancy food");
+contentH.textContent = "Welcome to the SUPER RESTAURANT!"
+
 
 content.appendChild(contentImg);
 content.appendChild(contentH);
