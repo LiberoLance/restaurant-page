34fe003ed3e3a4841ff9ea25b3f11a238190f7ef
Libero Lance <mfc2294@gmail.com>
2025-01-15 20:50:51 -0500

    set attribute alt for img on page

--- src/page.js
+++ src/page.js
@@ -4,6 +4,8 @@ const contentImg = document.createElement("img");
 const contentH = document.createElement("h1");
 const contentP = document.createElement("p");
 
+contentImg.setAttribute("alt", "A plate of fancy food");
+
 content.appendChild(contentImg);
 content.appendChild(contentH);
 content.appendChild(contentP);
