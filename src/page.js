8bec6d4c52bab4bd8bbd92eb6d396062dc389667
Libero Lance <mfc2294@gmail.com>
2025-01-20 15:48:21 -0500

    write export function to load page

--- src/page.js
+++ src/page.js
@@ -1,15 +1,19 @@
-const content = getElementById("content");
+function loadPage() {
+  const content = getElementById("content");
 
-const contentImg = document.createElement("img");
-const contentH = document.createElement("h1");
-const contentP = document.createElement("p");
+  const contentImg = document.createElement("img");
+  const contentH = document.createElement("h1");
+  const contentP = document.createElement("p");
 
-contentImg.setAttribute("alt", "A plate of fancy food");
-contentH.textContent = "Welcome to the SUPER RESTAURANT!"
-contentP.textContent = "Here at the super Restuarant we only serve the finest
-in fine dinning. Our Chefs are world reknown and expertly trained in dishing
-out the very best meals money can provide."
+  contentImg.setAttribute("alt", "A plate of fancy food");
+  contentH.textContent = "Welcome to the SUPER RESTAURANT!"
+  contentP.textContent = "Here at the super Restuarant we only serve the finest
+  in fine dinning. Our Chefs are world reknown and expertly trained in dishing
+  out the very best meals money can provide."
 
-content.appendChild(contentImg);
-content.appendChild(contentH);
-content.appendChild(contentP);
+  content.appendChild(contentImg);
+  content.appendChild(contentH);
+  content.appendChild(contentP);
+}
+
+export { loadPage };
