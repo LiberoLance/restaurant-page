11ae801d9a040f79f3557bd955cbf4fcd79fe35e
Libero Lance <mfc2294@gmail.com>
2025-01-23 20:18:26 -0500

    ditto

--- src/page.js
+++ src/page.js
@@ -7,9 +7,7 @@ function loadPage() {
 
   contentImg.setAttribute("alt", "A plate of fancy food");
   contentH.textContent = "Welcome to the SUPER RESTAURANT!";
-  contentP.textContent = "Here at the super Restuarant we only serve the finest
-  in fine dinning. Our Chefs are world reknown and expertly trained in dishing
-  out the very best meals money can provide.";
+  contentP.textContent = "Here at the super Restuarant we only serve the finestin fine dinning. Our Chefs are world reknown and expertly trained in dishing out the very best meals money can provide.";
 
   content.appendChild(contentImg);
   content.appendChild(contentH);
