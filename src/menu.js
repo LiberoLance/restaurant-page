e81920dcbed45c79e35e99291b2a522bd8d934c0
Libero Lance <mfc2294@gmail.com>
2025-02-10 09:08:47 -0500

    add container and header for sides

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,6 @@
 const page = document.createElement("div");
 const header = document.createElement("div");
+const beverageH = document.createElement("div");
 const beverages = document.createElement("div");
+const sidesH = document.createElement("div");
+const sides = doucment.createElement("div");
