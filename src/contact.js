1f1e6bddcdd8de3a42a49f4de74a7ad2e818e808
Libero Lance <mfc2294@gmail.com>
2025-03-15 15:19:23 -0400

    make func for loading contact type

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,8 @@
-function loadContact(){
+function loadContactPage(){
   const contactPageContainer = document.createElement("div");
   const titleContainer = document.createElement("div");
+
+  function loadContact(){
+    const contactContainer = document.createElement("div");
+  }
 }
