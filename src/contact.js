c4328c742c399075b290eca582c1127c616c08cb
Libero Lance <mfc2294@gmail.com>
2025-03-31 19:41:15 -0400

    add loop to make one contact node

--- src/contact.js
+++ src/contact.js
@@ -2,6 +2,21 @@ function loadContactPage(){
   const contactPageContainer = document.createElement("div");
   const titleContainer = document.createElement("div");
 
+  for(let i = 1; i < 3; i++) {
+    const contactContaineri = document.createElement("div");
+    const contactNamei = document.createElement("h1");
+    const contactuli = document.createElement("ul");
+    const contactTitlei = document.createElement("li");
+    const contactNumberi = document.createElement(li);
+    const contactEmaili = document.createElement("li");
+    
+    contactuli.appendChild(contactTitlei);
+    contactuli.appendChild(contactNumberi);
+    contactuli.appendChild(contactEmaili);
+    contactContaineri.appendChild(contactNamei);
+    contactContaineri.appendChild(contactuli);
+  }
+  
   function loadContact(){
     const contactContainer = document.createElement("div");
   }
