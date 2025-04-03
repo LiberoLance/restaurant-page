64e73b1c60ba5faaee918d074eaa7ed3a07bbded
Libero Lance <mfc2294@gmail.com>
2025-04-02 21:32:40 -0400

    move loop code to its own function

--- src/contact.js
+++ src/contact.js
@@ -21,6 +21,19 @@ function loadContactPage(){
   }
   
   function loadContact(){
-    const contactContainer = document.createElement("div");
+    for(let i = 1; i < 3; i++) {
+      const contactContaineri = document.createElement("div");
+      const contactNamei = document.createElement("h1");
+      const contactuli = document.createElement("ul");
+      const contactTitlei = document.createElement("li");
+      const contactNumberi = document.createElement(li);
+      const contactEmaili = document.createElement("li");
+
+      contactuli.appendChild(contactTitlei);
+      contactuli.appendChild(contactNumberi);
+      contactuli.appendChild(contactEmaili);
+      contactContaineri.appendChild(contactNamei);
+      contactContaineri.appendChild(contactuli);
+    }
   }
 }
