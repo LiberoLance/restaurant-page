279830fd7f70ed5d951ed50f9404fd14f137a824
Libero Lance <mfc2294@gmail.com>
2025-04-08 19:58:37 -0400

    call helper function

--- src/contact.js
+++ src/contact.js
@@ -3,24 +3,12 @@ function loadContactPage(){
   const titleContainer = document.createElement("div");
   const titleP = document.createElement("p");
   titleP.textContent = "Contact Us";
-  titleContainer.appendChild(titleP);  
+  titleContainer.appendChild(titleP);
 
-  for(let i = 1; i < 3; i++) {
-    const contactContaineri = document.createElement("div");
-    const contactNamei = document.createElement("h1");
-    const contactuli = document.createElement("ul");
-    const contactTitlei = document.createElement("li");
-    const contactNumberi = document.createElement(li);
-    const contactEmaili = document.createElement("li");
-    
-    contactuli.appendChild(contactTitlei);
-    contactuli.appendChild(contactNumberi);
-    contactuli.appendChild(contactEmaili);
-    contactContaineri.appendChild(contactNamei);
-    contactContaineri.appendChild(contactuli);
-  }
-  
-  function loadContact(){
+  loadContact();  
+}
+
+function loadContact(){
     for(let i = 1; i < 3; i++) {
       const contactContaineri = document.createElement("div");
       const contactNamei = document.createElement("h1");
@@ -36,4 +24,3 @@ function loadContactPage(){
       contactContaineri.appendChild(contactuli);
     }
   }
-}
